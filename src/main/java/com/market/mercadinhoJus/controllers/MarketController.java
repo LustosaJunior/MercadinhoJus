88cd57d4e70116852377cfe6cc9631654eade7bb
Decio Junior <de.junior@reply.com>
2024-08-09 05:39:54 -0300

    Adding message to show when access the page

--- src/main/java/com/market/mercadinhoJus/controllers/MarketController.java
+++ src/main/java/com/market/mercadinhoJus/controllers/MarketController.java
@@ -4,16 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(value = "/v1/market")
 public class MarketController {
 
     private static final Logger log = LoggerFactory.getLogger(MarketController.class);
 
-    @GetMapping(value = "/healthcheck")
+    @GetMapping(value = "healthcheck")
     public ResponseEntity<?> healthCheck() {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK, HttpStatus.OK);
     }
 }
