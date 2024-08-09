c9ef520010d0622a127e97aa48408735ef8fce46
Decio Junior <de.junior@reply.com>
2024-08-09 05:23:03 -0300

    Adding healthcheck request

--- src/main/java/com/market/mercadinhoJus/controllers/MarketController.java
+++ src/main/java/com/market/mercadinhoJus/controllers/MarketController.java
@@ -2,12 +2,18 @@ package com.market.mercadinhoJus.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MarketController {
 
     private static final Logger log = LoggerFactory.getLogger(MarketController.class);
 
-    @RequestMapping()
+    @GetMapping(value = "/healthcheck")
+    public ResponseEntity<?> healthCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
